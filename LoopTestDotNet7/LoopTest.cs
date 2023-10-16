d03cccdef8b42dde3b1df37f05b022b289e087be
Reza <reza.mansouri70@gmail.com>
2023-10-16 20:24:21 +0330

    Add project files.

---
+++ LoopTestDotNet7/LoopTest.cs
@@ -0,0 +1,32 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace LoopTestDotNet7
+{
+
+
+
+    [SimpleJob(RuntimeMoniker.Net70)]
+    [MemoryDiagnoser(false)]
+    public class Benchmarks
+    {
+        private List<int> ItemsList;
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            var random = new Random(420);
+            var randomItems = Enumerable.Range(0, 1000).Select(_ => random.Next());
+            ItemsList = randomItems.ToList();
+        }
+
+
+        [Benchmark]
+        public void ForEach_List()
+        {
+            foreach (var item in ItemsList)
+            {
+            }
+        }
+    }
+    }
---
+++ LoopTestDotNet7/Program.cs
@@ -0,0 +1,6 @@
+﻿using BenchmarkDotNet.Running;
+using LoopTestDotNet7;
+
+BenchmarkRunner.Run<Benchmarks>();
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
